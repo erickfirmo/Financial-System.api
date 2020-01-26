b49f874195137fc6c01659b428f57f293f1a100c
Erick <erickluques@gmail.com>
2020-01-26 20:24:31 -0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-new \Erickfirmo\DBConnection('../config/database.php');
+new \ErickFirmo\DBConnection('../config/database.php');
 
 session_start();
 
