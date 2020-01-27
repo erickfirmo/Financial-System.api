f000234262cc6cd8bc811ef7138fbef944c6d447
Erick <erickluques@gmail.com>
2020-01-27 00:22:15 -0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,4 +6,19 @@ new \ErickFirmo\DBConnection('../config/database.php');
 
 session_start();
 
-$route = (new Core\Router());
+$router = new \ErickFirmo\Router;
+$router->setNamespace('App\Controllers\\');
+
+
+$router->get(['/', 'HomeController@index']);
+
+$router->get(['/clientes', 'ClienteController@select']);
+$router->get(['/clientes/create/', 'ClienteController@create']);
+$router->get(['/clientes/{$id}/edit/', 'ClienteController@create']);
+$router->get(['/clientes/{$id}/', 'ClienteController@show']);
+
+$router->post(['/clientes/store/',  'ClienteController@store']);
+$router->put(['/clientes/{$id}/update', 'ClienteController@update']);
+$router->delete(['/clientes/{$id}/destroy', 'ClienteController@destroy']);
+
+$router->run();
