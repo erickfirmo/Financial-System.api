35508bdb3b99fc0772b016e28ff2e5e21dc45e55
Erick <erickluques@gmail.com>
2020-01-27 13:24:49 -0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ $router = new \ErickFirmo\Router;
 $router->setNamespace('App\Controllers\\');
 $router->get(['/', 'HomeController@index']);
 $router->get(['/clientes', 'ClienteController@index']);
-$router->get(['/clientes/create/', 'ClienteController@create']);
-$router->get(['/clientes/{$id}/edit/', 'ClienteController@create']);
 $router->get(['/clientes/{$id}/', 'ClienteController@show']);
 $router->post(['/clientes/store/',  'ClienteController@store']);
 $router->put(['/clientes/{$id}/update', 'ClienteController@update']);
