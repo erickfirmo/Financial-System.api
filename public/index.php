b0936a6960aa5d787a6868542881491204d1cfa3
Erick <erickluques@gmail.com>
2020-01-27 01:26:12 -0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,23 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-new \ErickFirmo\DBConnection('../config/database.php');
-
 session_start();
 
-$router = new \ErickFirmo\Router;
-$router->setNamespace('App\Controllers\\');
+$query_builder = (new \ErickFirmo\Model('../config/database.php'));
 
+require_once __DIR__.'/../routes/api.php';
 
-$router->get(['/', 'HomeController@index']);
-
-$router->get(['/clientes', 'ClienteController@index']);
-$router->get(['/clientes/create/', 'ClienteController@create']);
-$router->get(['/clientes/{$id}/edit/', 'ClienteController@create']);
-$router->get(['/clientes/{$id}/', 'ClienteController@show']);
-
-$router->post(['/clientes/store/',  'ClienteController@store']);
-$router->put(['/clientes/{$id}/update', 'ClienteController@update']);
-$router->delete(['/clientes/{$id}/destroy', 'ClienteController@destroy']);
 
 $router->run();
